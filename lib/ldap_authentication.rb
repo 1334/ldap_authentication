9bb33cd82f7053224b4243c3bfc9fc3f751dcfea
1334 <1334@shadowproject.net>
2013-06-06 21:29:58 +0200

    initial implementation

--- lib/ldap_authentication.rb
+++ lib/ldap_authentication.rb
@@ -1,5 +1,15 @@
-require "ldap_authentication/version"
+require 'ldap_authentication/version'
+require 'net/ldap'
 
 module LdapAuthentication
-  # Your code goes here...
+  def self.authenticate(name,password)
+    return false if password.blank?
+    user = "#{ENV['COMPANY_NAME']}\\#{name}"
+    ldap = Net::LDAP.new
+    ldap.host = "#{ENV['LDAP_ADDRESS']}"
+    ldap.port = "#{ENV['LDAP_PORT']}"
+    ldap.encryption(:simple_tls)
+    ldap.auth(user,password)
+    ldap.bind
+  end
 end
